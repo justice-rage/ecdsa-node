d7d948a56a8b9dc154cb2817e18c0ba9b9cfea4a
justicepelteir <justice.douglas@hey.com>
2023-01-05 15:33:08 -0800

    create transaction

--- server/scripts/hashAndSignMessage.js
+++ server/scripts/hashAndSignMessage.js
@@ -2,11 +2,11 @@ const { keccak256 } = require("ethereum-cryptography/keccak");
 const { utf8ToBytes, toHex } = require("ethereum-cryptography/utils");
 const secp = require("ethereum-cryptography/secp256k1");
 (async () => {
-    const PRIVATE_KEY = "cbcb60df4a5d4751ace0ac82fe3b393622c9c9f1184a656b905805974d9e06c5";
+    const PRIVATE_KEY = "74f4cd449c047999456528ba339a2c32eb12e2c9d3ba6b4708a2b12d242c40a6";
     let message = {
-        from: "0x9c27ac0f223616fa705dbdedf7b5524a126273b674441b038e21672823028bf1",
-        to: "0x4717d36ac94067dd722c7c9129f89ddf2f58076e3cf7da18161771d3325ee87f",
-        amount: 25,
+        from: "0x4717d36ac94067dd722c7c9129f89ddf2f58076e3cf7da18161771d3325ee87f",
+        to: "0x9c27ac0f223616fa705dbdedf7b5524a126273b674441b038e21672823028bf1",
+        amount: 10,
     };
     console.log("Message : ", message);
 
